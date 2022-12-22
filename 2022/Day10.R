8f78f0bf6d32a390da72756486c3f83d1cc8e23a
TeroJii <tmjalk@gmail.com>
2022-12-22 09:24:16 +0200

    add function for drawing the image

--- 2022/Day10.R
+++ 2022/Day10.R
@@ -34,4 +34,94 @@ rel_program_df %>%
 
 #### --- Part Two ---
 
-emptry_image <- matrix(rep(".",240), nrow = 240/40)
+# a function for drawing a line of pixels
+draw_pixels <- function(cycles = 1:240, df = program_df){
+  #draw pixels to this vector
+  pixels <- rep("", length(cycles))
+
+  for(i in seq_along(cycles)){
+    #for the first cycle x value = 1, so the first pixel is #
+    if(i == 1){
+      pixels[i] <- "#"
+    }
+
+    # get x value during each cycle
+    if(i>1){
+      x_val <- df[df$cycle_after_completed<=i,]$x %>% tail(1)
+    }
+
+    #for the first line of pixels excluding the first pixel
+    if(i >1 & i<=40){
+      # note: we are counting pixel rows from 1 to 40 here, not 0 to 39
+      draw_hash <- between(i, left = x_val, right = x_val + 2)
+      if(draw_hash){
+        pixels[i] <- "#"
+      } else {
+        pixels[i] <- "."
+      }
+    }
+
+    #for the second line of pixels
+    if(i > 40 & i<=80){
+      # note: we are counting pixel rows from 1 to 40 here, not 0 to 39
+      draw_hash <- between(i-40, left = x_val, right = x_val + 2)
+      if(draw_hash){
+        pixels[i] <- "#"
+      } else {
+        pixels[i] <- "."
+      }
+    }
+
+    #for the third line of pixels
+    if(i > 80 & i<=120){
+      # note: we are counting pixel rows from 1 to 40 here, not 0 to 39
+      draw_hash <- between(i-80, left = x_val, right = x_val + 2)
+      if(draw_hash){
+        pixels[i] <- "#"
+      } else {
+        pixels[i] <- "."
+      }
+    }
+
+    #for the fourth line of pixels
+    if(i > 120 & i<=160){
+      # note: we are counting pixel rows from 1 to 40 here, not 0 to 39
+      draw_hash <- between(i-120, left = x_val, right = x_val + 2)
+      if(draw_hash){
+        pixels[i] <- "#"
+      } else {
+        pixels[i] <- "."
+      }
+    }
+
+    #for the fifth line of pixels
+    if(i > 160 & i<=200){
+      # note: we are counting pixel rows from 1 to 40 here, not 0 to 39
+      draw_hash <- between(i-160, left = x_val, right = x_val + 2)
+      if(draw_hash){
+        pixels[i] <- "#"
+      } else {
+        pixels[i] <- "."
+      }
+    }
+
+    #for the sixth line of pixels
+    if(i > 200 & i<=240){
+      # note: we are counting pixel rows from 1 to 40 here, not 0 to 39
+      draw_hash <- between(i-200, left = x_val, right = x_val + 2)
+      if(draw_hash){
+        pixels[i] <- "#"
+      } else {
+        pixels[i] <- "."
+      }
+    }
+  } #end for loop
+
+  return(pixels)
+} # end draw_pixels
+
+# draw pixels from the program
+pixels <- draw_pixels()
+
+#draw the image
+matrix(pixels, nrow = 240/40)
